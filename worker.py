5587d6a6899671cb4b90c4b8f9278068aa650f57
etovityusha <numbernine27@gmail.com>
2022-12-10 20:26:12 +0400

    tg callback

--- worker.py
+++ worker.py
@@ -1,5 +1,7 @@
 from functools import lru_cache
+from json import dumps
 
+import requests
 from celery import Celery
 from pydantic import BaseSettings
 
@@ -30,12 +32,32 @@ class TaskException(Exception):
 celery_app = Celery(__name__, broker="redis://redis:6379/0")
 
 
+class TgBotSettings(BaseSettings):
+    tg_bot_token: str
+    tg_chat_id: str
+
+    class Config:
+        env_file = ".env"
+
+
+@lru_cache()
+def get_tg_bot_settings():
+    return TgBotSettings()
+
+
 @celery_app.task(name="refresh_stats", autoretry_for=(TaskException,), retry_kwargs={'max_retries': 7, 'countdown': 5})
-def refresh_stats(username: str):
+def refresh_stats(username: str, tg_callback: bool = True):
     try:
         stats = DefaultSharkScopeSvc(PyProxyService(get_settings().pyproxy_base_url)).get_statistic(username)
     except Exception as e:
         print(str(e)[:100])
         raise TaskException
     MongoStatsRepo(get_database()).set_statistic(username, StatisticEntity(**stats.dict()))
+    if tg_callback:
+        tg_bot_token = get_tg_bot_settings().tg_bot_token
+        tg_chat_id = get_tg_bot_settings().tg_chat_id
+        requests.get(
+            f"https://api.telegram.org/{tg_bot_token}/"
+            f"sendMessage?chat_id={tg_chat_id}&text={dumps(stats.dict(), indent=2)}"
+        )
     return True
